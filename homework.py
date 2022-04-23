e45115f08a80f402989492ccb2ba7cd7265d41a4
maks <mstrakulin@yandex.ru>
2022-04-23 15:03:56 +0300

    1 редактирование

--- homework.py
+++ homework.py
@@ -1,11 +1,14 @@
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    pass
+
+#0  message = print(f'Информация')
+#   return message
 
 
 class Training:
     """Базовый класс тренировки."""
-
+    LEN_STEP = 0.65
+    M_IN_KM = 1000
     def __init__(self,
                  action: int,
                  duration: float,
@@ -14,15 +17,19 @@ class Training:
         self.action = action
         self.duration = duration
         self.weight = weight
-        pass
+
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        pass
+        distance = training.action * Training.LEN_STEP / Training.M_IN_KM
+        return distance
+
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        pass
+        av_speed = training.get.distance / time_training
+        return av_speed
+
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
@@ -30,16 +37,24 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        pass
+        return InfoMessage.message
 
 
 class Running(Training):
     """Тренировка: бег."""
-    pass
+    coeff_calorie_1 = 18
+    coeff_calorie_2 = 20
+    def run (self, action , duration , weight ):
+        super().__init__(action,duration,weight)
+    def get_spent_calories(self) -> float:
+        return (Running.coeff_calorie_1 * training.get_mean_speed() - Running.coeff_calorie_2) / Training.M_IN_KM * time_training
+
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
+    def wall (self, action , duration , weight, height):
+        super().__init__(action,duration,weight)
     pass
 
 
@@ -50,11 +65,13 @@ class Swimming(Training):
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
+
     pass
 
 
 def main(training: Training) -> None:
     """Главная функция."""
+
     pass
 
 
@@ -68,4 +85,6 @@ if __name__ == '__main__':
     for workout_type, data in packages:
         training = read_package(workout_type, data)
         main(training)
+
+Training(13,45,34)
 
