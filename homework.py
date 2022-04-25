1be0af96606cc719125936b23b740880d0d85b39
ASU-StrakulinMS <mstrakulin@yandex.ru>
2022-04-25 16:09:30 +0300

    Работоспособность 9

--- homework.py
+++ homework.py
@@ -66,7 +66,8 @@ class Running(Training):
 
     def get_spent_calories(self) -> float:
         calories = (self.coeff_calorie_1 * self.get_mean_speed()) \
-            - self.coeff_calorie_2 * self.weight / self.M_IN_KM * (self.duration * 60)
+            - self.coeff_calorie_2 * self.weight / self.M_IN_KM \
+            * (self.duration * 60)
         return calories
 
 
